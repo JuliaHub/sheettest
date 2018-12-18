0ad734ac6d14cf4a199e43cd4e178773f81389e3
Yulia Pashneva <pashneva@advert.local>
2018-12-18 18:48:18 +0200

    first commit

---
+++ clock.py
@@ -0,0 +1,19 @@
+import run_hub
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+def my_job():
+    run_hub.step1()
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', minutes=2)
+def timed_job():
+    my_job()
+
+sched.start()
+
+
+
+    
+    
---
+++ run_hub.py
@@ -0,0 +1,17 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import datetime
+import os
+
+def step1():
+    scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+    file_name = os.environ['GOOGLE_APPLICATION_CREDENTIALS']
+    credentials =  ServiceAccountCredentials.from_json_keyfile_name(file_name, scope)
+
+    gc = gspread.authorize(credentials)
+    wks = gc.open('test2').sheet1
+    wks.clear()
+
+    data = str(datetime.datetime.now())
+    wks.append_row(['data'])
+    wks.append_row([data])
