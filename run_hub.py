63aa453df097d17b9dbf32a504551955095c0a08
Yulia Pashneva <pashneva@advert.local>
2018-12-19 09:55:10 +0200

    3

--- run_hub.py
+++ run_hub.py
@@ -5,8 +5,11 @@ import os
 
 def step1():
     scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-    file_name = os.environ['GOOGLE_APPLICATION_CREDENTIALS']
-    credentials =  ServiceAccountCredentials.from_json_keyfile_name(file_name, scope)
+    #file_name = os.environ['GOOGLE_APPLICATION_CREDENTIALS']
+    #credentials =  ServiceAccountCredentials.from_json_keyfile_name(file_name, scope)
+
+    credentials_dict = os.environ['CREDENTIALS_DICT']
+    credentials =  ServiceAccountCredentials.from_json_keyfile_dict(credentials_dict, scope)
 
     gc = gspread.authorize(credentials)
     wks = gc.open('test2').sheet1
